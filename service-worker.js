b9e542c8af75a9815413e97d1e3db06458d020ca
Sébastien Porati <sebastien.porati@gmail.com>
2017-08-29 12:23:26 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -3,16 +3,7 @@ self.addEventListener('install', function(event) {
   
   console.debug(event);
   
-  var options = {
-    body: "Service worker in progress",
-    vibrate: [200, 100, 200, 100, 200, 100, 400],
-    tag: "request",
-    actions: [
-      { action: "close", title: "Close"}
-    ]
-  };
-  
-  self.registration.showNotification("Install in progress", options);
+  askNotificationPermission();  
 });
 
 self.addEventListener('fetch', function(event) {
@@ -25,6 +16,23 @@ self.addEventListener('activate', function(event) {
   console.log("Activate event");
 
   console.debug(event);
+  
+  var options = {
+    body: "Service worker in progress",
+    vibrate: [200, 100, 200, 100, 200, 100, 400],
+    tag: "request",
+    actions: [
+      { action: "close", title: "Close"}
+    ]
+  };
+  
+  if ('actions' in Notification.prototype) {
+    // Action buttons are supported.
+  } else {
+    // Action buttons are NOT supported.
+  }
+  
+  self.registration.showNotification("Install in progress", options);
 });
 
 self.addEventListener('notificationclick', function(event) {
@@ -36,3 +44,20 @@ self.addEventListener('notificationclick', function(event) {
   // See: http://crbug.com/463146
   event.notification.close();
 });
+
+function askNotificationPermission() {
+  return new Promise(function(resolve, reject) {
+    const permissionResult = Notification.requestPermission(function(result) {
+      resolve(result);
+    });
+
+    if (permissionResult) {
+      permissionResult.then(resolve, reject);
+    }
+  })
+  .then(function(permissionResult) {
+    if (permissionResult !== 'granted') {
+      throw new Error('We weren\'t granted permission.');
+    }
+  });
+}
