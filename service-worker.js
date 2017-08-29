f31c6709d06764713fb5e16898ea26b88d471cbf
Sébastien Porati <sebastien.porati@gmail.com>
2017-08-29 12:13:35 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,6 +2,17 @@ self.addEventListener('install', function(event) {
   console.log("Install event");
   
   console.debug(event);
+  
+  var options = {
+    body: "Service worker in progress",
+    vibrate: [200, 100, 200, 100, 200, 100, 400],
+    tag: "request",
+    actions: [
+      { action: "close", title: "Close"}
+    ]
+  };
+  
+  self.registration.showNotification("Install in progress", options);
 });
 
 self.addEventListener('fetch', function(event) {
@@ -14,4 +25,14 @@ self.addEventListener('activate', function(event) {
   console.log("Activate event");
 
   console.debug(event);
+});
+
+self.addEventListener('notificationclick', function(event) {
+  console.log('On notification click: ');
+  
+  console.debug(event);
+  
+  // Android doesn’t close the notification when you click on it
+  // See: http://crbug.com/463146
+  event.notification.close();
 });
